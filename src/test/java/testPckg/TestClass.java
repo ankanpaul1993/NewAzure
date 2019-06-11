4c12b5e9ade114fee1c0eb7edcef6db9d8618504
789967 <789967@PC353574.cts.com>
2019-06-11 13:55:40 +0530

    first commit

---
+++ src/test/java/testPckg/TestClass.java
@@ -0,0 +1,32 @@
+package testPckg;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+
+
+public class TestClass {
+	public static WebDriver driver;
+
+	public static void main(String[] args) throws Exception {
+		
+      System.setProperty("webdriver.chrome.driver","C:\\Users\\789967\\eclipse-workspace\\TestAzure\\chromedriver.exe");
+      driver=new ChromeDriver();
+      
+      driver.get("https://www.google.com/");
+      def();
+      
+	}
+	
+	public static void def() throws Exception {
+		// TODO Auto-generated method stub
+		
+String j="hi";
+		
+		Assert.assertEquals("bye", j);
+		
+Thread.sleep(4000);
+
+}
+}
