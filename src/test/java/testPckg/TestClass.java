4a1e6ddddc13b1fb3a49283573a073c2681739cb
ankanpaul1993 <46998752+ankanpaul1993@users.noreply.github.com>
2019-06-25 12:30:30 +0530

    Update TestClass.java

--- src/test/java/testPckg/TestClass.java
+++ src/test/java/testPckg/TestClass.java
@@ -1,32 +1,33 @@
-package testPckg;
+package clms.testcase;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.chrome.ChromeDriver;
+	import org.testng.Assert;
+	import org.testng.annotations.Test;
 
 
 
-public class TestClass {
-	public static WebDriver driver;
-
-	public static void main(String[] args) throws Exception {
+	public class TestClass {
+		public static WebDriver driver;
+		@Test
+		public void browserLaunch() throws Exception {
+			
+	      System.setProperty("webdriver.chrome.driver","C:\\Users\\789967\\eclipse-workspace\\TestAzure\\chromedriver.exe");
+	      driver=new ChromeDriver();
+	      
+	      driver.get("https://www.google.com/");
+	      def();
+	      
+		}
 		
-      System.setProperty("webdriver.chrome.driver","C:\\Users\\789967\\eclipse-workspace\\TestAzure\\chromedriver.exe");
-      driver=new ChromeDriver();
-      
-      driver.get("https://www.google.com/");
-      def();
-      
+		public static void def() throws Exception {
+			// TODO Auto-generated method stub
+			
+	String j="hi";
+			
+			Assert.assertEquals("bye", j);
+			
+	Thread.sleep(4000);
+
 	}
-	
-	public static void def() throws Exception {
-		// TODO Auto-generated method stub
-		
-String j="hi";
-		
-		Assert.assertEquals("bye", j);
-		
-Thread.sleep(4000);
-
-}
-}
+	}
